84598db899314807e2209036d6106a71123888d8
scminy0527-rgb <scminy0527@gmail.com>
2026-05-31 16:36:16 +0900

    *fix: 커뮤니티 테스트 용 계정 수정

--- src/main/java/com/app/springapp/service/CommunityAuthServiceImpl.java
+++ src/main/java/com/app/springapp/service/CommunityAuthServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class CommunityAuthServiceImpl implements CommunityAuthService {
     @Override
     public Long getUserId() {
-        return 1L;
+        return 2L;
     }
 
 //    만약 userid 가 null 이면 예외 반환
